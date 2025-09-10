8f3f7640522d9d782ae3cb6e7e67514ae2898cb5
primika27 <kprimika@yahoo.com>
2025-09-10 09:54:46 -0400

    modif tests

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,14 +4,18 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
 def test_addition():
     my_calculator = Calculator()
-    assert my_calculator.add(2, 3) == 5
-    assert my_calculator.add(-1, 1) == 0
-    assert my_calculator.add(-1, -1) == -2
+    assert my_calculator.addition(2, 3) == 5
+    assert my_calculator.addition(-1, 1) == 0
+    assert my_calculator.addition(-1, -1) == -2
